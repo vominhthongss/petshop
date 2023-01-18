b9db3470d14fe7c639d89c5a2f6a787f269774b4
vominhthongss <vominhthongss@gmail.com>
2023-01-18 16:57:03 +0700

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <link rel="icon" type="image/png" href="{{ asset('favicon.png') }}" />
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Petshop</title>
+
+</head>
+
+<body class="antialiased">
+    <h1 class="text-3xl font-bold underline">
+        Hello world!
+    </h1>
+</body>
+
+</html>
