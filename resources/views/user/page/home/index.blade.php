f291cf93ee6b3c3ef159828442f26f35c3cde35a
vominhthongss <vominhthongss@gmail.com>
2023-01-29 16:41:06 +0700

    add banner

--- resources/views/user/page/home/index.blade.php
+++ resources/views/user/page/home/index.blade.php
@@ -181,4 +181,29 @@
             <span class="text-xl font-bold">Vệ sinh</span>
         </div>
     </div>
+    <div id="carouseProduct" class="carousel slide relative p-5" data-bs-ride="carousel">
+        <div class="carousel-inner relative w-full overflow-hidden grid grid-cols-3">
+            <div class="carousel-item active relative float-left w-full">
+                <img src="{{ asset('carousel/slide1.png') }}" class="block w-full" alt="pet1" />
+            </div>
+            <div class="carousel-item relative float-left w-full">
+                <img src="{{ asset('carousel/slide2.png') }}" class="block w-full" alt="pet2" />
+            </div>
+            <div class="carousel-item relative float-left w-full">
+                <img src="{{ asset('carousel/slide3.png') }}" class="block w-full" alt="pet3" />
+            </div>
+        </div>
+        <button
+            class="carousel-control-prev absolute top-0 bottom-0 flex items-center justify-center p-0 text-center border-0 hover:outline-none hover:no-underline focus:outline-none focus:no-underline left-0"
+            type="button" data-bs-target="#carouseProduct" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon inline-block bg-no-repeat" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+        </button>
+        <button
+            class="carousel-control-next absolute top-0 bottom-0 flex items-center justify-center p-0 text-center border-0 hover:outline-none hover:no-underline focus:outline-none focus:no-underline right-0"
+            type="button" data-bs-target="#carouseProduct" data-bs-slide="next">
+            <span class="carousel-control-next-icon inline-block bg-no-repeat" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+        </button>
+    </div>
 @endsection
